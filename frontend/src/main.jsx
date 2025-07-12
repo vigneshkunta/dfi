88718b68def5817a90d54a444e2649a20f193f6b
vignesh-yadav <vigneshyadav.dev@gmail.com>
2025-07-12 16:51:19 +0530

    feat: update main component to display payment required message and comment out App component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { Provider } from "react-redux";              
-import { store } from "./redux/store";                 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <Provider store={store}>                        
+      <Provider store={store}>
         <BrowserRouter>
-          <App />
+          {/* <App /> */}
+          <div class="flex items-center justify-center min-h-screen bg-yellow-100">
+            <div class="text-center bg-white border-4 border-yellow-500 text-yellow-700 p-10 rounded-lg shadow-xl max-w-xl w-full">
+              <h1 class="text-3xl font-bold mb-4">Payment Required</h1>
+              <p class="text-lg">
+                Please complete your payment to receive your website delivery.
+              </p>
+            </div>
+          </div>
         </BrowserRouter>
       </Provider>
     </HelmetProvider>
